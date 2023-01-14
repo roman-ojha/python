3c66d813d9d956a35f506fc5028a1951fe14d71c
Roman-Ojha <razzroman98@gmail.com>
2023-01-14 18:53:29 +0545

    refactor: some folder structure improvement & learned IIFE

---
+++ Tutorial/Main/core/118_Immediately_Invoked_Func_Expression/Immediately_Invoked_Func_Expression.py
@@ -0,0 +1,6 @@
+# Immediately Invoked Function Expressions (IIFE)
+# Python don't recommended it to use it that much
+
+# Syntax: (lambda <parameter...>: <expression>)(<arguments>...)
+
+(lambda x: print(x + 1))(5)
