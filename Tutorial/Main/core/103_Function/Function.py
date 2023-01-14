41232cb500dfc6e4e710baa110f4a6b0eee8c9af
Roman-Ojha <razzroman98@gmail.com>
2023-01-14 19:38:06 +0545

    refactor: seperated Function & Docstring tutorial

--- Tutorial/Main/core/103_Function/Function.py
+++ Tutorial/Main/core/103_Function/Function.py
@@ -33,19 +33,3 @@ v1 = function1(5, 7)
 # return the value of a,b and assing in the v
 print(v1)
 # printing v
-
-
-def function2(a, b):
-    """This is a function which will calculate average of two numbers
-    this function doesnt work for three numbers"""
-    # if you put the content inside it """ """  in the function then it will called as a docstring
-    # in a function string written in the first line is called as docstring
-    average = (a+b)/2
-    # print(average)
-    return average
-
-
-v = function2(5, 7)
-print(v)
-print(function2.__doc__)
-# we are accessing docstring using this syntax
