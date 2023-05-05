31e3972a13837dfe1e62d9f6728dd4f9248dba36
Roman-Ojha <razzroman98@gmail.com>
2023-05-05 09:52:31 +0545

    docs: some example of with ^& without typing

---
+++ Tutorial/Main/Core/129_Type_Hinting_And_Annotations/Type_Hinting_And_Annotations.py
@@ -0,0 +1,21 @@
+"""
+    -> Here we will going to use typing module to add type hint & annotations in python
+    *) Typing:
+        -> here by adding typing will not going to change the functionality of you make code it will not help to hint and annotate on you python code
+        -> you can say just like typescript
+        -> It kind of is for documentation purposes
+"""
+
+# here 'x' is int
+x = 1
+# here 'x' is string
+x = "roman"
+# we can do this because it is dynamically type
+
+# but we can add type annotation and hint to this variable by:
+y: str = 1 # here we can see '1' is and int but we are annotating it as str but we would not get an error because this is just of hinting and documenting
+name: str = "roman"
+
+"""
+    *) Static code Analysis
+"""
